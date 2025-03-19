fdce6d1ac5a95d755d233d5f545f5b0e7d777476
Jasper_Johansen <anoopanoop2001@gmail.com>
2025-03-19 22:59:32 +0530

    add more variables

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 const display = document.querySelector(".display");
-const btns =document.querySelectorAll("button");
+const btns =document.querySelectorAll("button[id^='btn']");
+const btnNumbers = Array.from(btns).filter(btn => /\d/.test(btn.id));
+const btnOperators = Array.from(btns).filter(btn => !/\d/.test(btn.id));
+
+const firstNumber='';
+const secondNumber='';
+const operator = '';
 
 function add(a,b){
     return a+b;
@@ -17,9 +23,10 @@ function divide(a,b){
     return a/b;
 }
 
-display.textContent ='';
-btns.forEach((btn) =>{
+
+btnNumbers.forEach((btn) =>{
     btn.addEventListener("click", () =>{
     display.textContent += btn.textContent;
     });
 });
+
